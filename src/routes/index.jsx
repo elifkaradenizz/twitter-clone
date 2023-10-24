d537c27d0fc5805b6fe7629fef589c722bf477be
Elif Karadeniz <81620156+elifkaradenizz@users.noreply.github.com>
2023-10-24 14:43:26 +0300

    anasayfanın genel yapısı oluşturuldu

---
+++ src/routes/index.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "~/layouts/main";
+import Explore from "~/pages/explore";
+import Home from "~/pages/home";
+import NotFound from "~/pages/not-found";
+import Notifications from "~/pages/notifications";
+
+const routes= createBrowserRouter([
+    {
+        path: '/',
+        element:<MainLayout/>,
+        children:[
+            { //main layoutta home özel olduğu için bu şekil kullanıyoruz..
+                index: true, 
+                element: <Home/>
+
+            },
+            {
+                path: 'explore',
+                element: <Explore/>
+            }, 
+            
+            {
+                path: 'notifications',
+                element: <Notifications/>
+            },
+            {
+                path: '*',
+                element: <NotFound/>
+            }
+        ]
+    },
+
+])
+
+export default routes
